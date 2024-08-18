044ae67a0dccfe4e09da934c2c275b6b577c0e70
Usuario <eliasbenitez882@proton.me>
2024-08-17 21:48:31 -0400

    El juego ya esta completo

--- ahorcados/ahorcados.py
+++ ahorcados/ahorcados.py
@@ -1,4 +1,32 @@
 import random
-palabras = ["escuela", "universidad", "compuratora", "impresora"]
-palabra_random = random.choise(palabras)
+import os
+palabras = ["universidad","computadora","impresora","escritorio"]
 
+palabra_random = random.choice(palabras)
+
+palabra_secreta = ["_"] * len(palabra_random)
+
+intentos = 8
+
+while intentos > 0 and "_" in palabra_secreta:
+    print (" ".join(palabra_secreta))
+    print (f"Te quedan {intentos} intentos.") 
+    
+    letra_usuario = input("Ingrese una letra: ").lower()
+
+    if letra_usuario in palabra_random:
+        for p, l in enumerate(palabra_random):
+            if l == letra_usuario:
+                palabra_secreta[p] = letra_usuario
+        print ("Correcto!.")
+        input("Enter para continuar.")
+        os.system("cls")
+    else:
+        intentos -= 1
+        print("Lo siento esa letra no esta en la palabra.")
+        input("Enter para continuar.")
+        os.system("cls")
+if "_" in palabra_secreta:
+    print(f"Te quedaste sin Intentos.\n\nLa palabra era: {palabra_random}")
+else:
+    print(f"Felicidades, adivinaste la palabra: {palabra_random}\nY te sobraron {intentos} intetos")  
